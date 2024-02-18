30bffa0c560512f8350804b9071d24a4dc38e479
abiaki997-tech <abiakhil69@gmail.com>
2024-02-18 18:44:29 +0530

    port updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const mysql = require('mysql2/promise'); // Use the promise-based version
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Create a connection to the local MySQL database
 const db = mysql.createPool({
